8350bb229c3ab3f6a368af998e67a1b8e555a82a
AmalUSAG <141402163+AmalUSAG@users.noreply.github.com>
2023-08-28 09:13:09 +0400

    Modifying manifest

--- uasg_custom_budget/__manifest__.py
+++ uasg_custom_budget/__manifest__.py
@@ -25,8 +25,8 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
         'security/budget_security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/res_users.xml'
