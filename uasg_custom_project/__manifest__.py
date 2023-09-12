de01292222259485fda75d3c84dbef0f81e64136
AmalUASG <141402163+AmalUASG@users.noreply.github.com>
2023-09-12 11:31:29 +0400

    Add files via upload

---
+++ uasg_custom_project/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "UASG Project",
+
+    'summary': """
+            Customization for UASG Project
+""",
+
+    'description': """
+            Customization for UASG Project
+
+    """,
+
+    'author': "UASG / Amal Abelmajid",
+    'website': "https://www.alsaqergroup.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','mail'],
+
+    # always loaded
+    'data': [
+        'data/data.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    
+    'installable': True,
+    'application': True,
+}
