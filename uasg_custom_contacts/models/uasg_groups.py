655ae6bc01bcf6459df47e64ec4d620c505bba14
AmalUSAG <141402163+AmalUSAG@users.noreply.github.com>
2023-11-09 12:15:59 +0400

    adding uasg custom contacts

--- uasg_custom_contacts/models/uasg_groups.py
+++ uasg_custom_contacts/models/uasg_groups.py
@@ -14,7 +14,7 @@ class UasgContacts(models.Model):
     email = fields.Char()
     mobile = fields.Char()
     #odoo_groups = fields.mayny
-    company = fields.Char()
+    company = fields.Char(compute='_update_user_company')
     department = fields.Char(compute='_update_user_depatment')
     job_title= fields.Char()
     user = fields.Many2one('res.users' , compute = '_link_with_res_users' , store=True)
@@ -30,7 +30,6 @@ class UasgContacts(models.Model):
         return True
 
     @api.depends('uasg_id')
-
     def _update_user_depatment (self):
 
         config = self.env['ad.configuration'].search([('active','=',True)],limit=1)
@@ -59,6 +58,45 @@ class UasgContacts(models.Model):
                 department = response_department.json().get('value')
 
                 contact.write({'department' : department})
+
+            else :
+
+                contact.write({'department' : ""})
+
+    @api.depends('uasg_id')
+    def _update_user_company (self):
+
+        config = self.env['ad.configuration'].search([('active','=',True)],limit=1)
+
+        tenant_id = config.tenant_id
+        client_id = config.client_id
+        client_secret = config.client_secret
+        headers = {"Content-type": "application/x-www-form-urlencoded"}
+        payload = str('grant_type=client_credentials&client_secret='+str(client_secret)+'&client_id='+str(client_id)+'&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default')
+        url = str("https://login.microsoftonline.com/"+str(tenant_id)+"/oauth2/v2.0/token")
+        req = requests.request("POST" , url,headers=headers,data = payload)
+        req = req.json()
+        access_token = req.get('access_token')
+        headers = {'Content-Type': 'application/json','Authorization' : access_token }
+
+
+        for contact in self :
+
+            get_user_company = str('https://graph.microsoft.com/v1.0/users/'+str(contact.uasg_id)+'/companyName')
+
+            response_company = requests.request("GET" , get_user_company,headers=headers)
+
+            
+            if response_company.json().get('value') : 
+
+                company = response_company.json().get('value')
+
+                contact.write({'company' : company})
+
+            else :
+
+                contact.write({'company' : ""})
+
 
 
 
