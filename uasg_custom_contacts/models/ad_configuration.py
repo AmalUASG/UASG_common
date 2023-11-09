2968d7ee3f7b4c98792707174911e3a67db9aec0
AmalUSAG <141402163+AmalUSAG@users.noreply.github.com>
2023-11-09 09:26:34 +0400

    adding uasg custom contacts

--- uasg_custom_contacts/models/ad_configuration.py
+++ uasg_custom_contacts/models/ad_configuration.py
@@ -96,7 +96,7 @@ class AdConfiguration(models.Model):
         req = requests.request("POST" , url,headers=headers,data = payload)
         req = req.json()
         access_token = req.get('access_token')
-        get_members_url = str('https://graph.microsoft.com/v1.0/users')
+        get_members_url = str('https://graph.microsoft.com/v1.0/users?$top=999')
         headers = {'Content-Type': 'application/json','Authorization' : access_token }
         response = requests.request("GET" , get_members_url,headers=headers)
         members=response.json().get('value')
