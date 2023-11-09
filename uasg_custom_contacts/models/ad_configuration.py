4b4d670d8313cf9ffc4840d297fcdff5530bce9f
AmalUSAG <141402163+AmalUSAG@users.noreply.github.com>
2023-11-09 11:07:07 +0400

    adding uasg custom contacts

--- uasg_custom_contacts/models/ad_configuration.py
+++ uasg_custom_contacts/models/ad_configuration.py
@@ -178,8 +178,9 @@ class AdConfiguration(models.Model):
 
             get_user_company = str('https://graph.microsoft.com/v1.0/users/'+str(contact.uasg_id)+'/companyName')
             response_company = requests.request("GET" , get_user_company,headers=headers)
-
-            contact.write({'company' : response_company.json().get('value')})
+            if str(response_company.json().get('value')) : 
+                raise UserError(str(response_company.json().get('value')))
+                contact.write({'company' : str(response_company.json().get('value'))})
 
 
 
