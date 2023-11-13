5ddd3ac4a413e12e6539c7647812b5dbaefa19a6
AmalUSAG <141402163+AmalUSAG@users.noreply.github.com>
2023-11-13 12:03:57 +0400

    updates

--- uasg_custom_contacts/models/uasg_groups.py
+++ uasg_custom_contacts/models/uasg_groups.py
@@ -14,14 +14,14 @@ class UasgContacts(models.Model):
     email = fields.Char()
     mobile = fields.Char()
     #odoo_groups = fields.mayny
-    company = fields.Char(compute='_update_user_company' )
+    company = fields.Char(compute='_update_user_company' , store=True)
     department = fields.Char(compute ='_update_user_depatment' )
     job_title= fields.Char()
     user = fields.Many2one('res.users' , compute = '_link_with_res_users' , store=True )
     # company_id = Many2one('res.company' , compute = '_link_with_res_company' , store=True)
     manager_name  = fields.Char(compute ='_get_user_manager' )
     manager_email = fields.Char(compute ='_get_user_manager' )
-    company_id = fields.Many2one('res.company',compute='_get_res_company')
+    company_id = fields.Many2one('res.company',compute='_get_res_company',store=True)
     
     @api.depends('company')
     def _get_res_company(self):
