bea516224c8faa630911fa782a39e22b88272523
AmalUSAG <141402163+AmalUSAG@users.noreply.github.com>
2023-11-13 09:56:13 +0400

    updates

--- uasg_custom_contacts/models/res_users.py
+++ uasg_custom_contacts/models/res_users.py
@@ -10,14 +10,12 @@ class ResUsers(models.Model):
     uasg_contact = fields.Many2one('uasg.contacts',compute='_link_with_uasg_contacts')
     uasg_department = fields.Char(related='uasg_contact.department')
 
-
+    @api.depends('login')
     def _link_with_uasg_contacts (self) :
 
     	for record in self :
-
-            user = record.env['uasg.contacts'].search([])
-            for u in user :
-                if u.email == record.login :
-                    record.uasg_contact  = u.id
-                else :
-                    return True
+            user = record.env['uasg.contacts'].search([('email','=',record.login)])
+            if user :     
+                    record.uasg_contact  = user.id
+            else :
+                return True
