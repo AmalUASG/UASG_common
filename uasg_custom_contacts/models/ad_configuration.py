a352260085527573fb0691bd92487245d156c92c
AmalUSAG <141402163+AmalUSAG@users.noreply.github.com>
2023-11-15 14:01:08 +0400

    u

--- uasg_custom_contacts/models/ad_configuration.py
+++ uasg_custom_contacts/models/ad_configuration.py
@@ -154,7 +154,7 @@ class AdConfiguration(models.Model):
         req = req.json()
         access_token = req.get('access_token')
 
-        last_update_date = datetime.strptime(self.write_date,'%Y-%m-%dT%H:%M:%S.%fZ')
+        last_update_date = datetime.strptime(str(self.write_date),'%Y-%m-%dT%H:%M:%S.%fZ')
         get_members_url = str('https://graph.microsoft.com/v1.0/users?$filter=accountEnabled%20eq%20true&onPremisesLastSyncDateTime ge' + last_update_date +'&$select=displayName,mail,id,mobilePhone,jobTitle,companyName,department&$top=999')
         raise UserError(str(get_members_url))
         headers = {'Content-Type': 'application/json','Authorization' : access_token }
