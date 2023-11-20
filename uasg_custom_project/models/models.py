62cd437f8d5aec546270aacf58245224f23d7ddf
AmalUSAG <141402163+AmalUSAG@users.noreply.github.com>
2023-11-20 09:34:12 +0400

    update

--- uasg_custom_project/models/models.py
+++ uasg_custom_project/models/models.py
@@ -124,14 +124,14 @@ class UASGProject(models.Model):
         })
 
         headers = {"Content-type": "application/json"}
-        payload = str('response_type=code&client_id=2e98a997-764b-41e6-976f-4451a215e063&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&redirect_uri=https%3A%2F%2Fapp.uasg.ae%2Fauth_oauth%2Fsignin')
+        payload = str('response_mode=form_post&response_type=code&client_id=2e98a997-764b-41e6-976f-4451a215e063&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&redirect_uri=https%3A%2F%2Fapp.uasg.ae%2Fauth_oauth%2Fsignin')
         url = str("https://login.microsoftonline.com/58481125-7f09-407d-921a-dc425b00fd0f/oauth2/v2.0/authorize") 
         req = requests.request("GET" , url,headers=headers,data=payload)
         # req = req.json()
         # raise UserError(str(req))
         # req = req.json
         # access_token = request.GET.get('token')
-        # raise UserError(str(req))
+        raise UserError(str(req.text))
 
         url = "https://graph.microsoft.com/v1.0/me/sendMail"
         # headers = {'Content-Type': 'application/json','Authorization' : access_token }
