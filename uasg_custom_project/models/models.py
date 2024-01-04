37d4f5903062db23937f551e36c2e3d9568e76af
AmalUSAG <141402163+AmalUSAG@users.noreply.github.com>
2024-01-04 16:20:06 +0400

    project updtes archive /department

--- uasg_custom_project/models/models.py
+++ uasg_custom_project/models/models.py
@@ -74,7 +74,7 @@ class UASGProject(models.Model):
 
             if record.create_uid :
 
-                if record.department == self.env.uasg_department :
+                if record.department == self.env.user.uasg_department :
 
                     record.is_user_department = True
                 else :
